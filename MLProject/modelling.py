d326a2322f4546161a5dad8dce4194d471ffe1b4
andyputrapratama-cmd <andyputrapratama@students.unnes.ac.id>
2026-07-21 18:22:47 +0700

    Fix nested run error in python script

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -18,7 +18,7 @@ def load_data():
 def train_model():
     X_train, X_test, y_train, y_test = load_data()
     
-    with mlflow.start_run():
+    with mlflow.start_run(nested=True):
         model = RandomForestClassifier(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
         
